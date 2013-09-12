c5b6890e98a1917b7a6d90d52493825f157ea564
Gabe <gabe@paperlesspost.com>
2013-09-12 12:47:10 -0400

    use folder structure based on gatherer set codes

--- script/dump_images.rb
+++ script/dump_images.rb
@@ -3,28 +3,28 @@ require 'fileutils'
 require 'multi_json'
 require 'open-uri'
 
-def sets
+def raw_sets
   path = File.expand_path('../../data/sets.json', __FILE__)
-  File.open(path, 'r') do |file|; return MultiJson.load(file.read); end
+  File.open(path, 'r'){|file| MultiJson.load(file.read)}
 end
-
-def mgci_sets
-  @mgci_sets ||= sets.select{|s| s['mgci_code']}
-end
-
-def mgci_code(name)
-  (@mgci_codes ||= {})[name] ||= # memoize return value
-    mgci_sets.find{|s| s['name'] == name}['mgci_code'] rescue nil
-end
-
-def cards
+def raw_cards
   path = File.expand_path('../../data/cards.json', __FILE__)
-  File.open(path, 'r') do |file|; return MultiJson.load(file.read); end
+  File.open(path, 'r'){|file| MultiJson.load(file.read)}
 end
 
-cards.each do |card|
-  next unless set_code = mgci_code(card['set_name'])
-  puts uri = "http://magiccards.info/scans/en/#{set_code}/#{card['collector_num']}.jpg"
-  FileUtils.mkdir_p dir = File.join('data', 'images', '312x445', set_code)
+def sets
+  @sets ||= raw_sets.select do |set|
+    set['mgci_code'] && set['gatherer_code']
+  end
+end
+def get_set(name)
+  (@_sets ||= {})[name] ||= # memoize return value
+    sets.find{|s| s['name'] == name}
+end
+
+raw_cards.each do |card|
+  next unless set = get_set(card['set_name'])
+  puts uri = "http://magiccards.info/scans/en/#{set['mgci_code']}/#{card['collector_num']}.jpg"
+  FileUtils.mkdir_p dir = File.join('data', 'images', '312x445', set['gatherer_code'])
   File.open(File.join(dir, File.basename(uri)), 'wb'){|f| f.write(open(uri).read)}
 end
