562d10938d8bb789709d1ac9e142083feb1b5629
Gabe <gabe@paperlesspost.com>
2013-09-26 16:35:27 -0400

    substitutions to filter set data

--- script/gatherer/dump_sets.rb
+++ script/gatherer/dump_sets.rb
@@ -4,7 +4,9 @@ FILE_PATH = File.expand_path('../../../data/gatherer/sets.json', __FILE__)
 
 def extract_data(item)
   name = item.text
+  return unless substitute(name)
   return unless code = set_code(name)
+  name = substitute(name)
   {
     'name' => name,
     'gatherer_code' => code
@@ -16,6 +18,16 @@ def set_code(name)
   set_url = "http://gatherer.wizards.com/Pages/Search/Default.aspx?set=[\"#{name}\"]"
   img = get(set_url).css("img").find{|i| i.attr(:title).try(:match, name)}
   CGI.parse(img.attr(:src))['set'].first.try(:downcase) if img
+end
+
+def substitute(name)
+  case name
+  when "Magic: The Gathering-Commander"
+    "Commander"
+  when "Promo set for Gatherer"
+    nil
+  else name
+  end
 end
 
 page = get('http://gatherer.wizards.com/Pages/Default.aspx')
