5731f6840e54828ff45c2f7350daa70face08199
Gabe <gabe@paperlesspost.com>
2013-12-10 11:51:22 -0500

    fix paths for image dump

--- script/mgci/dump_images.rb
+++ script/mgci/dump_images.rb
@@ -4,11 +4,11 @@ require 'multi_json'
 require 'open-uri'
 
 def raw_sets
-  path = File.expand_path('../../data/mgci/sets.json', __FILE__)
+  path = File.expand_path('../../../data/mgci/sets.json', __FILE__)
   File.open(path, 'r'){|file| MultiJson.load(file.read)}
 end
 def raw_cards
-  path = File.expand_path('../../data/mgci/cards.json', __FILE__)
+  path = File.expand_path('../../../data/mgci/cards.json', __FILE__)
   File.open(path, 'r'){|file| MultiJson.load(file.read)}
 end
 
@@ -27,7 +27,7 @@ raw_cards.each do |card|
   next if ARGV[0] && ARGV[0] != set['mgci_code']
   uri = "http://magiccards.info/scans/en/#{set['mgci_code']}/#{card['collector_num']}.jpg"
   begin; data = open(uri).read
-    FileUtils.mkdir_p dir = File.join('data', 'images', 'mgci (312x445)', set['mgci_code'])
+    FileUtils.mkdir_p dir = File.join('data', 'images', 'mgci_312x445', set['mgci_code'])
     File.open(File.join(dir, File.basename(uri)), 'wb'){|f| f.write(data)}; print '.'
   rescue OpenURI::HTTPError => e
     puts "\nFAILED: #{set['mgci_code']}/#{card['collector_num']}.jpg - #{e}"
