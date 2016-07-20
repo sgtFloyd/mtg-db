dd66c9692560291125bd4f92c1324e4a46ab219d
Gabe <gabe@paperlesspost.com>
2016-07-20 12:44:58 -0400

    dump set attributes concurrently

--- script_v2/dump_sets.rb
+++ script_v2/dump_sets.rb
@@ -1,10 +1,32 @@
 require_relative './script_util.rb'
 
 FILE_PATH = File.expand_path('../../data_v2/sets.json', __FILE__)
+EXCLUDED_SETS = %w[Vanguard]
+THREAD_POOL_SIZE = 25
+
+def extract_attrs(set_names)
+  set_names.each_slice(THREAD_POOL_SIZE).map do |slice|
+    slice.map do |set_name|
+      Thread.new do
+        set_page = get "http://gatherer.wizards.com/Pages/Search/Default.aspx?set=[%22#{set_name}%22]"
+        set_img = set_page.css('img[src^="../../Handlers/Image.ashx?type=symbol&set="]').first
+        set_code = set_img.attr(:src)[/set=(\w+)/, 1].downcase
+        { 'name' => set_name, 'gatherer_code' => set_code }
+      end
+    end.map(&:join).map(&:value) # Wait for threads to finish and grab results
+  end.flatten(1) # Flatten slices
+end
+
+def merge(set_json)
+  existing = Hash[read(FILE_PATH).map{|s| [s['name'], s]}]
+  set_json.each do |set|
+    existing[set['name']] = (existing[set['name']] || {}).merge(set)
+  end
+  existing.values
+end
 
 page = get "http://gatherer.wizards.com/Pages/Default.aspx"
-sets = page.css('select[name$="setAddText"] option').map(&:text).reject(&:empty?)
-sets.each do |set|
-  set_page = get "http://gatherer.wizards.com/Pages/Search/Default.aspx?set=[%22#{set}%22]"
-  require 'pry'; binding.pry
-end
+sets = page.css('select[name$="setAddText"] option').map(&:text)
+sets.reject!(&:empty?).reject!{|name| name.in?(EXCLUDED_SETS)}
+
+write FILE_PATH, merge(extract_attrs(sets)).sort_by{|set| set['name']}
--- script_v2/script_util.rb
+++ script_v2/script_util.rb
@@ -8,6 +8,9 @@ class Object
   def try(*a, &b)
     __send__(*a, &b) unless self.nil?
   end
+  def in?(enumerable)
+    enumerable.include?(self)
+  end
 end
 
 def get(url)
