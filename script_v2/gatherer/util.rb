5019f548e84191b8dd8afbc41610218812af9eaa
Gabe <gabe@paperlesspost.com>
2017-01-10 16:12:35 -0500

    1.0.12: include missing CN2 Conspiracies in dump

--- script_v2/gatherer/util.rb
+++ script_v2/gatherer/util.rb
@@ -14,6 +14,7 @@ class Gatherer
     if for_card
       "http://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid=#{for_card}"
     elsif for_set
+      # TODO: Append &action=advanced&special=true to return Conspiracies, Schemes, etc.
       "http://gatherer.wizards.com/Pages/Search/Default.aspx?sort=cn+&output=compact&set=[%22#{for_set}%22]"
     else
       "http://gatherer.wizards.com/Pages/Default.aspx"
@@ -53,10 +54,14 @@ class Gatherer
     # s00#13 is missing from Gatherer. This will pull the data from CARD_JSON_OVERRIDES
     multiverse_ids << 's00#13' if set_name == 'Starter 2000'
 
-    # CNS 1-13 (Conspiracies) missing from Gatherer search, but multiverse_ids exist
+    # CNS 1-13 (Conspiracies) missing from standard search, but multiverse_ids exist
     if set_name == 'Magic: The Gathering—Conspiracy'
-      multiverse_ids += [382222, 382253, 382289, 382315, 382360, 382393, 382411,
-                          382356, 382206, 382292, 382355, 382216, 382332]
+      multiverse_ids = [382222, 382253, 382289, 382315, 382360, 382393, 382411,
+        382356, 382206, 382292, 382355, 382216, 382332] + multiverse_ids
+    # CN2 1-12 (Conspiracies) missing from standard search, but multiverse_ids exist
+    elsif set_name == 'Conspiracy: Take the Crown'
+      multiverse_ids = [416758, 416759, 416760, 416761, 416762, 416763, 416764,
+        416765, 416766, 416767, 416768, 416769] + multiverse_ids
     end
     multiverse_ids
   end
