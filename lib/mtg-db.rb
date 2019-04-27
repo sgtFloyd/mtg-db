6f08e14cdbf95c68c61ee375a115025e754a43fe
Gabe <gabe@paperlesspost.com>
2019-04-26 23:43:25 -0400

    v1.4.2 War of the Spark (WAR)

--- lib/mtg-db.rb
+++ lib/mtg-db.rb
@@ -2,7 +2,7 @@ require 'json'
 
 module Mtg
   module Db
-    VERSION = '1.4.1'
+    VERSION = '1.4.2'
     CARDS_PATH = File.expand_path('../../data/sets/%s.json', __FILE__)
     SETS_PATH = File.expand_path('../../data/sets.json', __FILE__)
 
