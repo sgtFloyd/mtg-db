423d6cc7906f495ec3aae9f6bdba60d4b2e0430f
Gabe <gabe@paperlesspost.com>
2019-05-06 00:56:40 -0400

    2.1.1: update ath and dkm with rulings and latest Oracle changes

--- lib/mtg-db.rb
+++ lib/mtg-db.rb
@@ -2,7 +2,7 @@ require 'json'
 
 module Mtg
   module Db
-    VERSION = '2.1.0'
+    VERSION = '2.1.1'
     CARDS_PATH = File.expand_path('../../data/sets/%s.json', __FILE__)
     SETS_PATH = File.expand_path('../../data/sets.json', __FILE__)
 
